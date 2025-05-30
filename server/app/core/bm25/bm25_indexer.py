dc10dce438bdd8732150425d6fc3cc5c59f6b8a0
Frostedpilot <Truong.VT225535@sis.hust.edu.vn>
2025-05-30 13:54:18 +0700

    aaa

--- server/app/core/bm25/bm25_indexer.py
+++ server/app/core/bm25/bm25_indexer.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 from elasticsearch.helpers import bulk
 from app.core.utils.es_client import get_es_client
 
+
 def index_bm25_data(index_name: str, csv_path: str, mapping: dict):
     es = get_es_client()
 
@@ -13,8 +14,11 @@ def index_bm25_data(index_name: str, csv_path: str, mapping: dict):
         print(f"ℹ️ Index '{index_name}' already exists")
 
     df = pd.read_csv(csv_path, encoding="utf-8-sig")
-    df.dropna(subset=["content"], inplace=True)
-    df.drop_duplicates(subset=["content"], inplace=True)
+    df["content_full"] = (
+        df["from"] + "\n" + df["chapter"] + "\n" + df["section"] + "\n" + df["content"]
+    )
+    df.dropna(subset=["content", "content_full"], inplace=True)
+    df.drop_duplicates(subset=["content", "content_full"], inplace=True)
     docs = df.to_dict(orient="records")
 
     actions = [
@@ -25,7 +29,7 @@ def index_bm25_data(index_name: str, csv_path: str, mapping: dict):
                 "id": doc["id"],
                 "chuong": doc["chuong"],
                 "dieu": doc["dieu"],
-                "content": doc["content"]
+                "content": doc["content_full"],
             },
         }
         for doc in tqdm(docs)
